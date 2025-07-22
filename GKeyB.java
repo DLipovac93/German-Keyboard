52988dc45aa1c211f7ce36e14c547f663aeac78a
Damir Lipovac <LipovacD93@gmail.com>
2025-07-22 00:35:47 -0500

    Added buttons to board

--- GKeyB.java
+++ GKeyB.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class GKeyB implements ActionListener {
     JFrame board;
     JTextField textField;
-    JButton[] alphabeButtons = new JButton[30];
+    JButton[] alphabetButtons = new JButton[30];
     JButton[] functionButtons = new JButton[3];
     JButton A, B, C;
     JButton clr, ply, spc;
@@ -27,5 +27,42 @@ public class GKeyB implements ActionListener {
         board.setSize(1325, 500);
         board.setLayout(null);
         board.setResizable(true);
+
+        textField = new JTextField();
+        textField.setBounds(10, 340, 920, 100);
+        textField.setFont(myFont);
+        textField.setEditable(false);
+        textField.setBorder(border);
+
+        A = new JButton("A");
+        B = new JButton("B");
+        C = new JButton("C");
+
+        clr = new JButton("Clear");
+        ply = new JButton("Play");
+        spc = new JButton("_");
+
+        alphabetButtons[0] = A;
+        alphabetButtons[1] = B;
+        alphabetButtons[2] = C;
+        functionButtons[0] = clr;
+        functionButtons[1] = ply;
+        functionButtons[2] = spc;
+
+        for (int i = 0; i < 29; i++) {
+            alphabetButtons[i].addActionListener(this);
+            alphabetButtons[i].setFont(myFont);
+            alphabetButtons[i].setFocusable(false);
+            alphabetButtons[i].setBounds(10 + (i % 10) * (120 +10), 10 + (i / 10) * (100 + 10), 120, 100);
+            alphabetButtons[i].setBorder(border);
+            board.add(alphabetButtons[i]);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+            functionButtons[i].setBorder(border);
+        }
     }
 }
